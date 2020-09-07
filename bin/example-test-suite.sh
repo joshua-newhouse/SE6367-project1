cf0038e7e637996122eeec5b36d635924aa90c79
Joshua Newhouse <joshua.b.newhouse@gmail.com>
2020-09-07 11:21:02 -0500

    Fixed example test suite

--- bin/example-test-suite.sh
+++ bin/example-test-suite.sh
@@ -1,12 +1,5 @@
 #Spell test suite
 
-#Additional logging for tests
-T_GREEN='\033[1;32m'
-T_RED='\033[1;31m'
-
-SuccessMessage='eval echo -e "${T_GREEN}$(date +"%Y-%m-%d %T") [SUCCESS] - ${FUNCNAME[0]},${LINENO[0]}: "'
-FailureMessage='eval echo -e "${T_RED}$(date +"%Y-%m-%d %T") [FAILURE] - ${FUNCNAME[0]},${LINENO[0]}: "'
-
 # Test cases
 
 function Test1() {
